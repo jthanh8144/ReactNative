079ab3d8b71960a677939e23118f088401276e7e
VoVanThanh <thanhvo618@gmail.com>
2022-02-17 16:14:38 +0700

    ex1

--- Test/App.js
+++ Test/App.js
@@ -4,7 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <View style={styles.containerTop}>
+        <View style={styles.top}>
+          <Text style={styles.topText}>Top Left</Text>
+        </View>
+      </View>
+      <View style={styles.containerCenter}>
+        <View style={styles.center}>
+          <Text style={styles.centerText}>Center</Text>
+        </View>
+      </View>
+      <View style={styles.containerBottom}>
+        <View style={styles.bot}>
+          <Text style={styles.botText}>Bottom Right</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +27,58 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#e7feff',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  containerTop: {
+    flex: 1,
+  },
+  top: {
+    marginTop: 80,
+    marginHorizontal: 40,
+    padding: 20,
+    borderWidth: 4,
+    borderColor: 'red',
+    backgroundColor: 'pink'
+  },
+  topText: {
+    fontSize: 20,
+    color: '#509cb8',
+    fontWeight: 'bold'
+  },
+  containerCenter: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  center: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#33ff60',
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerText: {
+    fontSize: 20,
+    color: '#509cb8',
+    fontWeight: 'bold'
+  },
+  containerBottom: {
+    flex: 1,
+  },
+  bot: {
+    marginTop: 80,
+    marginHorizontal: 40,
+    padding: 20,
+    borderRadius: 8,
+    backgroundColor: '#7ee6fd',
+
+  },
+  botText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'right',
+  }
 });
